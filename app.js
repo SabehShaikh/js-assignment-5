5d7517726255c5017fe85547d3722a915f0cda17
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-02-18 17:44:43 +0500

    added updated javascript file

--- app.js
+++ app.js
@@ -50,19 +50,67 @@ document.write("Score of " + students[1] + " is " + score[1] + " Percentage is "
 document.write("Score of " + students[2] + " is " + score[2] + " Percentage is " + (score[2] / 500) * 100 + "<br>");
 
 // Question 09
-
 document.write("<h4> Question 09 </h4>")
-// initialize the array with color names
-var colors = ["red", "green", "blue"];
 
-// display the array in the browser
-document.write("Initial array: " + colors + "<br>");
+// 9. Initialize an array with color names. Display the array elements in your browser.
 
-// ask the user for a color to add to the beginning of the array
-var newColor = prompt("Enter a color to add to the beginning of the array:");
+var colors = ["Red", "Green", "Blue"];
+document.write("Colors are :" + " " + colors + "<br>");
 
-// add the new color to the beginning of the array
-colors.unshift(newColor);
+// a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array. 
+// Display the updated array in your browser.
 
-// display the updated array in the browser
-document.write("Updated array: " + colors);
+var newColorStart = prompt("Enter the color name you want to add in the beginning");
+colors.unshift(newColorStart);
+
+document.write("New colors after adding in the beginning are :" + " " + colors + "<br>");
+
+// b. Ask the user what color he/she wants to add to the end & add that color to the end of the array.
+// Display the updated array in your browser.
+
+var newColorEnd = prompt("Enter the color name you want to add in the end");
+colors.push(newColorEnd)
+
+document.write("New colors after adding in end are :" + " " + colors + "<br>");
+
+// c. Add two more color to the beginning of the array. Display the updated array in your browser.
+
+colors.unshift("Yellow", "White");
+
+document.write("Updated colors after adding two more colors in the beginning are :" + " " + colors + "<br>");
+
+// d. Delete the first color in the array. Display the updated array in your browser.
+
+colors.shift();
+document.write("Updated colors after deleting first color are :" + " " + colors + "<br>");
+
+// e. Delete the last color in the array. Display the updated array in your browser.
+
+colors.pop();
+document.write("Updated colors after deleting last color are :" + " " + colors + "<br>");
+
+// f. Ask the user at which index he/she wants to add a color & color name.
+// Then add the color to desired position/index. . Display the updated array in your browser.
+
+var userInput = prompt("At which index do you want to add a color, and which color?");
+var inputArray = userInput.split(" ");
+
+var index = parseInt(inputArray[0]);
+var color = inputArray[1]
+
+colors.splice(index, 0, color);
+
+document.write("Updated colors are: " + colors + "<br>");
+
+// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete.
+// Then remove the same number of color(s) from user-defined position/index. . Display the updated array in your browser.
+
+var user = prompt("At which index do you want to delete a color, and how many colors?");
+var inputArray = user.split(" ");
+
+var index = parseInt(inputArray[0]);
+var numColors = parseInt(inputArray[1]);
+
+colors.splice(index, numColors);
+
+document.write("Updated colors are: " + colors);
